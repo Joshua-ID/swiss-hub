29964b3d48287e6083c7fcd9d1618e9df09c32c2
Joshua-ID <160030880+Joshua-ID@users.noreply.github.com>
2025-12-08 15:55:40 +0100

    refactor: improve layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -21,31 +21,10 @@ import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-// Loading component
-// import LoadingSpinner from "./components/LoadingSpinner";
-// import { useStore } from "./store/useStore";
-// import { useStore } from "zustand";
-
 function App() {
-  // const { currentUser, isLoading, fetchCurrentUser } = useStore();
-
-  // // Fetch user data on app load
-  // useEffect(() => {
-  //   fetchCurrentUser();
-  // }, [fetchCurrentUser]);
-
-  // // Show loading spinner while checking authentication
-  // if (isLoading) {
-  //   return (
-  //     <div className="min-h-screen flex items-center justify-center">
-  //       <LoadingSpinner size="lg" />
-  //     </div>
-  //   );
-  // }
-
   return (
     <Router>
-      <div className="min-h-screen w-full flex flex-col">
+      <div className="min-h-screen w-full   flex flex-col">
         <Navbar />
         <main className="grow">
           <Routes>
